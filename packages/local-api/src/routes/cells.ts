5e135a58598cdc73809ab72de2cfe01944b78571
Itta Kato <itta.kato17@gmail.com>
2023-03-29 20:42:47 +0900

    add initial state for cells

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -30,8 +30,10 @@ function createCellsRouter(filename: string, dir: string) {
     } catch (error) {
       if (isLocalApiError(error)) {
         if (error.code === 'ENOENT') {
-          await fs.writeFile(fullPath, '[]', 'utf-8');
-          res.send([]);
+          const initialState =
+            '[{"content":"# js-notebook\\n\\njs-notebook is a web-based notebook environment for interactive computing of JavaScript. Markdown is available for comprehensive documentation of code.\\n\\n- A **Markdown cell** contains text formatted using Markdown and displays its output in-place when the Markdown cell is run. Click to edit.\\n\\n- A **code cell** contains JavaScript code to be executed. When the code is run, the notebook displays the output on the right hand side by using the show() function.\\n\\n- ```show``` function is a built-in function that can used to display outputs including React components.\\n\\n- Variables and functions can be referred to in the subsequent cells\\n\\n- Add a new cell by hovering on the divider between each cell\\n\\n- Click on the format button on the top-right of the code cell to format code\\n\\nAll changes are saved in a file you opened with. By default, the filename **notebook.js`** is used.\\n\\n\\n\\n\\n","type":"text","id":"f5d3304a-25a2-4cda-a586-a52060ac8cec"},{"content":"import { useState } from \'react\';\\n\\nfunction Counter() {\\n  const [count, setCount] = useState(0);\\n\\n  function onClickHandler() {\\n    setCount((prevCount) => prevCount + 1);\\n  }\\n\\n  return (\\n    <div>\\n      <p>You clicked {count} times</p>\\n      <button onClick={onClickHandler}>Increment</button>\\n    </div>\\n  );\\n}\\n\\nshow(<Counter />);","type":"code","id":"c76d1080-8341-4e7a-8342-5355da2e5c1d"}]';
+          await fs.writeFile(fullPath, initialState, 'utf-8');
+          res.send(initialState);
         }
       } else {
         throw error;
